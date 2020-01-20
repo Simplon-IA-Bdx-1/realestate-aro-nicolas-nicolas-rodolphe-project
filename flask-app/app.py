c27433fcd484a13c64d035a1c004e31832f5565b
nicoOkie <chapon.nicola@gmail.com>
2020-01-20 21:54:09 +0100

    prompting the needed features

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index():  
+    return 'Hello World!'
 
 if __name__ == "__main__":
     app.run()
