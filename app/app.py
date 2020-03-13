411193fa60744b59418d3704ed68c406921f0951
Nicolas Autexier <nicolas.atx@gmx.fr>
2020-03-13 16:52:56 +0100

    up

--- app/app.py
+++ app/app.py
@@ -3,49 +3,35 @@ from joblib import load
 import pandas as pd
 import numpy as np
 
-# x = pd.DataFrame()
-# x.loc[0,"type_de_bien"] = 'maison'
-# x.loc[0,"nb_de_pieces"] = 4
-# x.loc[0,"surface"] = 100
-# x.loc[0,"ville"] = "BORDEAUX"
-# x.loc[0, "p_m2"] = 1000
-input_data = pd.DataFrame.from_dict({'type_de_bien': ['maison'], 'nb_de_pieces': [4], 'surface': [100], 'ville': ['BORDEAUX'], 'p_m2': [1000]})
-
-model = load('./models/model_xgb_v3.joblib')
-print(model.get_params().keys())
-# print(input_data)
-# print(x)
-output = model.predict(input_data)
-print(output)
-
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
-# @app.route('/predict')
-# def get_predict():
-#     return "COuocu ejlkdjlqksjdlKQKSDJFKJQLFDS"
+@app.route('/predict')
+def get_predict():
+    return "COuocu ejlkdjlqksjdlKQKSDJFKJQLFDS"
 
-# @app.route('/predict', methods=['POST'])
-# def post_predict():
-#     # input_data = [[
-#     #     request.form['sepal-length'],
-#     #     request.form['sepal-width'],
-#     #     request.form['petal-length'],
-#     #     request.form['petal-width']
-#     # ]]
+@app.route('/predict', methods=['POST'])
+def post_predict():
+    # input_data = [[
+    #     request.form['sepal-length'],
+    #     request.form['sepal-width'],
+    #     request.form['petal-length'],
+    #     request.form['petal-width']
+    # ]]
 
-#     input_data = pandas.DataFrame.from_dict({'type_de_bien': ['maison'], 'nb_de_pieces': ['4'], 'surface': ['100'], 'ville': ['BORDEAUX'], 'p_m2': ['1000']})
-#     print(input_data)
-#     model = load('./models/model_xgb_v3.joblib')
-#     print(model)
-#     output = model.predict(input_data)
-#     print(output)
+    input_data = pd.DataFrame.from_dict({'type_de_bien': ['maison'], 'nb_de_pieces': ['4'], 'surface': ['100'], 'ville': ['BORDEAUX'], 'p_m2': ['1000']})
+ 
+    model = load('./models/model_xgb_v3.joblib')
 
-#     return "Salut Alex"
+    output = model.predict(input_data)
+    output = np.exp(output)
+    print(output)
 
-# if __name__ == '__main__':
-#     app.run()
+    return str(output)
+
+if __name__ == '__main__':
+    app.run()
