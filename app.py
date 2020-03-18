f80ec6d6de65d56fc816963451cd460b64858071
Aro Razafindrakola <aro.razafindrakola@gmail.com>
2020-03-18 19:36:02 +0100

    works with model

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request
-import pickle
+import joblib
 import pandas as pd
+import math
+import os
+
 
 import webbrowser
-from threading import Timer
 
 webbrowser.open_new('http://127.0.0.1:5000')
+
+# print(input_data)
+# print(output)
+# os.system("pause")
+# exit()
 
 app = Flask(__name__)
 
@@ -21,17 +28,18 @@ def get_predict():
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    input_data = {"type_de_bien":[request.form['type_de_bien']],
-        "année":[request.form['annee']],
+    input_data = {
+        "type_de_bien":[request.form['type_de_bien']],
         "nb_de_pieces":[request.form['nb_de_pieces']],
         "surface":[request.form['surface']],
-        "ville":[request.form['ville']]
+        "ville":[request.form['ville']],
+        "p_m2":[request.form['p_m2']]
         }
     input_data = pd.DataFrame(input_data)
 
-    model = pickle.load(open('./model/HPv2.sav', 'rb'))
+    model = joblib.load("./model/model_xgb_v3.joblib")
     output = model.predict(input_data)
-
+    output = math.exp (output)
     return render_template('predict.html', classe = output)
 
 if __name__ == '__main__':
