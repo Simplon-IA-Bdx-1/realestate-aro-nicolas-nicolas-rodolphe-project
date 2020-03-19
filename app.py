583645659d4199ced33470667fda1fbe87371349
nicoOkie <chapon.nicola@gmail.com>
2020-03-19 15:20:07 +0100

    ready for v1

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-import requests
-from Cli.cli import Cli
-from production_ia import Model
-import locale
-
-locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
-locale._override_localeconv = {'mon_thousands_sep': ' '}
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
+import math
+import os
 
 
-def sanitize_data(data):
-    for key in data.keys():
-        data[key] = data[key].strip()
+import webbrowser
 
-    data['type_de_bien'] = data['type_de_bien'].lower()
-    data['ville'] = data['ville'].upper()
+webbrowser.open_new('http://127.0.0.1:5000')
 
-    return data
+# print(input_data)
+# print(output)
+# os.system("pause")
+# exit()
 
+app = Flask(__name__)
 
-cli = Cli()
-model = Model()
-
-data = sanitize_data(cli.prompt())
-prediction = (model.make_prediction(data))
-fr_price = locale.format_string('%.2f', prediction, grouping=True, monetary= True)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
 
+@app.route('/predict')
+def get_predict():
+    return render_template('get-prediction.html')
 
-print(f'Votre bien est estimé à : {fr_price}€')
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    input_data = {
+        "type_de_bien":[request.form['type_de_bien']],
+        "nb_de_pieces":[request.form['nb_de_pieces']],
+        "surface":[request.form['surface']],
+        "ville":[request.form['ville']],
+        "p_m2":[request.form['p_m2']]
+        }
+    input_data = pd.DataFrame(input_data)
+
+    model = joblib.load("./model/model_xgb_v3.joblib")
+    output = model.predict(input_data)
+    output = math.exp (output)
+    return render_template('predict.html', classe = output)
+
+if __name__ == '__main__':
+    app.run(debug=False)
