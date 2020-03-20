fac299d36c44946e326fe35ac8c0a3342ffc33a4
Nico-Facto <53934339+Nico-Facto@users.noreply.github.com>
2020-03-20 13:13:04 +0100

    prise en charge native du p_m2 par le modéle

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ import joblib
 import pandas as pd
 import math
 import os
+import statistics
 
 
 import webbrowser
 
 webbrowser.open_new('http://127.0.0.1:5000')
-
-# print(input_data)
-# print(output)
-# os.system("pause")
-# exit()
 
 app = Flask(__name__)
 
@@ -25,22 +21,48 @@ def hello_world():
 def get_predict():
     return render_template('get-prediction.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
+
+    df = joblib.load("./model/dataBrick_for_v3.joblib")
+    k4 = [request.form['ville']]
+
+    k4 = str(k4[0].upper())
+    print(f"--------------------{k4}-------------")
+
+    sclol = df[df['ville'].isin([f"{k4}"])]
+    if len(sclol) < 4 :
+        niv_confidence = "Faible, l'IA connait peut cette ville"
+    elif len(sclol) >3 and len(sclol) <10 : 
+        niv_confidence = "Normal, l'IA connait cette ville, mais manque un peu d'information"
+    elif len(sclol) >=10 : 
+        niv_confidence = "Forte, l'IA connait bien cette ville"
+    if len(sclol) != 0 :
+        k5 = statistics.mean(sclol["p_m2"])
+    else :
+        niv_confidence = "Faible, l'IA connait pas cette ville"
+        k5 = statistics.mean(df["p_m2"])
 
     input_data = {
         "type_de_bien":[request.form['type_de_bien']],
         "nb_de_pieces":[request.form['nb_de_pieces']],
         "surface":[request.form['surface']],
-        "ville":[request.form['ville']],
-        "p_m2":[request.form['p_m2']]
+        "ville":k4,
+        "p_m2":k5,
         }
+
     input_data = pd.DataFrame(input_data)
+    print(input_data)
 
     model = joblib.load("./model/model_xgb_v3.joblib")
     output = model.predict(input_data)
     output = math.exp (output)
-    return render_template('predict.html', classe = output)
+    output = int(output)
+
+    ui_display_var = f"{output}, confiance :  {niv_confidence}"
+
+    return render_template('predict.html', classe = ui_display_var)
 
 if __name__ == '__main__':
     app.run(debug=False)
